ef674a9f0c54629d225972bc419dee1ce6d5d3fb
yanghyeonjin <guswls7081@gmail.com>
2020-09-22 16:25:23 +0900

    * 정대리 - #14 SearchView

--- app/src/main/java/com/yanghyeonjin/hellokotlin/PhotoCollectionActivity.kt
+++ app/src/main/java/com/yanghyeonjin/hellokotlin/PhotoCollectionActivity.kt
@@ -1,18 +1,35 @@
 package com.yanghyeonjin.hellokotlin
 
+import android.app.SearchManager
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputFilter
 import android.util.Log
+import android.view.Menu
+import android.view.View
+import android.widget.CompoundButton
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.GridLayoutManager
 import com.yanghyeonjin.hellokotlin.adapter.PhotoGridViewAdapter
 import com.yanghyeonjin.hellokotlin.databinding.ActivityPhotoCollectionBinding
 import com.yanghyeonjin.hellokotlin.model.Photo
 
-class PhotoCollectionActivity : AppCompatActivity() {
+class PhotoCollectionActivity : AppCompatActivity(),
+                                SearchView.OnQueryTextListener,
+                                CompoundButton.OnCheckedChangeListener,
+                                View.OnClickListener {
     private lateinit var binding: ActivityPhotoCollectionBinding
 
     private var photoList = ArrayList<Photo>()
     private lateinit var photoGridViewAdapter: PhotoGridViewAdapter
+
+    // 서치뷰와 서치뷰의 EditText
+    private lateinit var searchView: SearchView
+    private lateinit var etSearchView: EditText
 
     companion object {
         const val TAG: String = "로그"
@@ -43,5 +60,104 @@ class PhotoCollectionActivity : AppCompatActivity() {
             this.adapter = photoGridViewAdapter
         }
 
+
+
+        // 액티비티에서 어떤 액션바를 사용할지 설정한다.
+        setSupportActionBar(binding.topAppBar)
+
+
+        binding.switchSearchHistorySaveMode.setOnCheckedChangeListener(this)
+        binding.btnDeleteAllHistory.setOnClickListener(this)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.top_app_bar, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView = menu?.findItem(R.id.menu_search)?.actionView as SearchView
+        searchView.apply {
+            this.queryHint = "검색어를 입력해주세요"
+            this.setOnQueryTextFocusChangeListener { view, hasExpanded ->
+                when(hasExpanded) {
+                    true -> {
+                        Log.e(TAG, "PhotoCollectionActivity - 서치뷰가 열렸다.")
+                        binding.linearSearchHistory.visibility = View.VISIBLE
+                    }
+                    false -> {
+                        Log.e(TAG, "PhotoCollectionActivity - 서치뷰가 닫혔다.")
+                        binding.linearSearchHistory.visibility = View.INVISIBLE
+                    }
+                }
+            }
+            this.setOnQueryTextListener(this@PhotoCollectionActivity)
+
+
+            etSearchView = this.findViewById(androidx.appcompat.R.id.search_src_text) // 서치뷰에서 EditText를 가져온다.
+
+        }
+
+        etSearchView.apply {
+            this.filters = arrayOf(InputFilter.LengthFilter(12))
+            this.setTextColor(Color.WHITE)
+            this.setHintTextColor(Color.WHITE)
+        }
+
+        return true
+    }
+
+
+    // 서치뷰 검색어 입력 이벤트
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        Log.e(TAG, "PhotoCollectionActivity - query: $query")
+
+        // 검색한 결과로 app bar title 다시 바꾸기
+        if (!query.isNullOrEmpty()) {
+            binding.topAppBar.title = query
+        }
+
+//        this.searchView.setQuery("", false)
+//        this.searchView.clearFocus()
+
+        binding.topAppBar.collapseActionView()
+
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.e(TAG, "PhotoCollectionActivity - newText: $newText")
+
+        val userInputText = newText ?: ""
+
+        if (userInputText.count() == 12) {
+            Toast.makeText(this, "검색어는 12자까지만 입력가능합니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        return true
+    }
+
+
+    // 스위치 변경 이벤트
+    override fun onCheckedChanged(switch: CompoundButton?, isChecked: Boolean) {
+        when(switch) {
+            binding.switchSearchHistorySaveMode -> {
+                if (isChecked) {
+                    Log.e(TAG, "검색어 저장 On")
+                } else {
+                    Log.e(TAG, "검색어 저장 Off")
+                }
+            }
+        }
+    }
+
+    // 뷰 클릭 이벤트
+    override fun onClick(view: View?) {
+        when (view) {
+            binding.btnDeleteAllHistory -> {
+                Log.e(TAG, "전체삭제 버튼이 클릭되었다.")
+            }
+        }
     }
 }
